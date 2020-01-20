cbadd48ed4d7dacf17d306f5d29ca6edf164724b
Frog <shmily40686@gmail.com>
2020-01-20 01:34:24 -0800

    Add routes for collections

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+const db = require('./config/keys').mongoURI;
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+const bodyParser = require('body-parser');
+
+mongoose
+    .connect(db, {userNewUrlParser: true})
+    .then(() => {
+        console.log('Connected to Mongo db...');
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
 });
+
+app.use("/api/users", users);
+
+app.use("/api/tweets", tweets);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
